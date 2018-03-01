a3ce757ede62838f9150570fc14e1717e7536ffe
Kevin T <kevin.tai.mail@gmail.com>
2018-03-01 12:36:00 -0800

    created collection view, cell, image view in storyboard

---
+++ Cats/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Cats
+//
+//  Created by KevinT on 2018-03-01.
+//  Copyright © 2018 KevinT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Cats/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Cats
+//
+//  Created by KevinT on 2018-03-01.
+//  Copyright © 2018 KevinT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
